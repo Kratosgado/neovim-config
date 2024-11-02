98690d2a277cd8144992cbfc68cafa3dd9e76734
Kratosgado <mbeahessilfieprince@gmail.com>
2024-11-02 16:09:44 +0000

    first commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,9 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+local opts = { noremap = true, silent = true }
+
+vim.api.nvim_set_keymap("n", ";;", "<ESC>", opts) -- normal mode
+vim.api.nvim_set_keymap("i", ";;", "<ESC>", opts) -- insert mode
+vim.api.nvim_set_keymap("v", ";;", "<ESC>", opts) -- visual mode
+vim.api.nvim_set_keymap("c", ";;", "<ESC>", opts) -- command-line mode
