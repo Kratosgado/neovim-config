e0da5541d4c2e7fcbadd71f8087bfb190c6b3fe5
Kratosgado <mbeahessilfieprince@gmail.com>
2024-11-04 14:12:02 +0000

    update configs

--- push.sh
+++ push.sh
@@ -2,6 +2,7 @@
 -- script to push configs to git repo
 
 cp ~/.bashrc ~/.config/nvim/terminal/. -rf
+cp ~/.config/startship.toml ~/.config/nvim/terminal/startship.toml
 cp ~/.profile ~/.config/nvim/terminal/.
 cp ~/.config/starship.toml ~/.config/nvim/terminal/. -rf
 
