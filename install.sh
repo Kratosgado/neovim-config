09ee8f5fd87417b12fc267514a1847ca8d301b5a
Kratosgado <mbeahessilfieprince@gmail.com>
2024-11-21 07:12:35 +0000

    remove profiles

--- install.sh
+++ install.sh
@@ -2,7 +2,3 @@
 
 mkdir -p ~/.config/
 cp -rf . ~/.config/nvim
-cp ./terminal/starship.toml ~/.config/starship.toml
-cp ./terminal/startship.toml ~/.config/startship.toml
-cp ./terminal/.bashrc ~/.bashrc
-cp ./terminal/.profile ~/.profile
