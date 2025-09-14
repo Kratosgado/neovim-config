9c33d7f457c904b950823a19f5d2d6f01335df75
Kratosgado <mbeahessilfieprince@gmail.com>
2025-09-14 11:28:59 +0000

    add neogit

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,8 +3,12 @@
 -- Add any additional options here
 vim.g.lazyvim_python_lsp = "pyright"
 vim.g.lazyvim_python_ruff = "ruff"
+-- set theme options
+LazyVim.config.colorscheme = "tokyonight-night"
 vim.opt.clipboard = "unnamedplus"
 -- Set to "bacon-ls" to use bacon-ls instead of rust-analyzer.
 vim.g.lazyvim_rust_diagnostics = "rust-analyzer"
 -- vim.g.lazyvim_picker = "fzf"
 -- vim.g.fzf_layout = { window = { scrollbar = 0 } }
+-- set wait time to 500ms
+-- vim.opt.timeoutlen = 100
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -15,7 +15,15 @@ return {
         "lua",
         "markdown",
         "markdown_inline",
+
+        "git_config",
+        "gitcommit",
+        "git_rebase",
+        "gitignore",
+        "gitattributes",
+
         "python",
+        "vue",
         "query",
         "regex",
         "tsx",
