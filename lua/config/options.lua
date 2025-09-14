24db84df85f9c208ef3288c54c7cf578f75f01a5
Kratosgado <mbeahessilfieprince@gmail.com>
2025-09-14 11:44:13 +0000

    fix: nvim-cmp popup

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,6 +6,8 @@ vim.g.lazyvim_python_ruff = "ruff"
 -- set theme options
 LazyVim.config.colorscheme = "tokyonight-night"
 vim.opt.clipboard = "unnamedplus"
+vim.g.ai_cmp = true
+vim.opt.pumblend = 0
 -- Set to "bacon-ls" to use bacon-ls instead of rust-analyzer.
 vim.g.lazyvim_rust_diagnostics = "rust-analyzer"
 -- vim.g.lazyvim_picker = "fzf"
