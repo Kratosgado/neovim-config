1ed6b8853759ab66f073a30d6ee1c2578b5b8c9c
Kratosgado <mbeahessilfieprince@gmail.com>
2025-09-14 11:56:54 +0000

    I'm loving it

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -4,6 +4,17 @@ return {
     keys = {
       { "<leader>gg", "<cmd>Neogit<cr>", desc = "Neogit" },
     },
+    opts = {
+      popup = { kind = "floating" },
+      integrations = { fzf_lua = true },
+      commit_editor = { kind = "floating" },
+      commit_select_view = { kind = "floating" },
+      commit_view = { kind = "floating" },
+      log_view = { kind = "floating" },
+      description_editor = { kind = "floating" },
+      reflog_view = { kind = "floating" },
+      tag_editor = { kind = "floating" },
+    },
     dependencies = {
       "nvim-lua/plenary.nvim", -- required
       "sindrets/diffview.nvim", -- optional - Diff integration
