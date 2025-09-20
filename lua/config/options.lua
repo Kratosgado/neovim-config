4708f166d1833317e1eaadd10f465dcd881a6f56
Kratosgado <mbeahessilfieprince@gmail.com>
2025-09-20 11:19:39 +0000

    refactor(master): replace nvim-cmp with blink

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -8,6 +8,7 @@ LazyVim.config.colorscheme = "tokyonight-night"
 vim.opt.clipboard = "unnamedplus"
 vim.g.ai_cmp = true
 vim.opt.pumblend = 0
+vim.g.lazyvim_blink_main = false
 -- Set to "bacon-ls" to use bacon-ls instead of rust-analyzer.
 vim.g.lazyvim_rust_diagnostics = "rust-analyzer"
 vim.g.lazyvim_picker = "fzf"
