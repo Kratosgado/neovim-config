8caf03ff38d5d8fb75960532570aea58fb69f329
Kratosgado <mbeahessilfieprince@gmail.com>
2025-09-28 02:35:59 +0000

    add(master): wtf

--- lua/plugins/minifiles.lua
+++ lua/plugins/minifiles.lua
@@ -1,24 +1,4 @@
 return {
-  -- {
-  --   "nvim-mini/mini.files",
-  --   version = false,
-  --   config = function()
-  --     require("mini.files").setup({
-  --       windows = {
-  --         preview = true,
-  --         width_focus = 30, -- Width of the focused window
-  --         width_preview = 80,
-  --       },
-  --       options = {
-  --         use_as_default_explorer = true,
-  --       },
-  --       mappings = {
-  --         close = ";;", -- Close file explorer
-  --         open = "<CR>", -- Open file
-  --       },
-  --     })
-  --   end,
-  -- },
   {
     "nvim-mini/mini.diff",
     event = "VeryLazy",
---
+++ lua/plugins/wtf.lua
@@ -0,0 +1,62 @@
+return {
+  --TODO: add settings
+  {
+    "piersolenski/wtf.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      -- "folke/snacks.nvim",
+      "ibhagwan/fzf-lua",
+    },
+    keys = {
+      {
+        "<leader>cwd",
+        mode = { "n", "x" },
+        function()
+          require("wtf").diagnose()
+        end,
+        desc = "Debug diagnostic with AI",
+      },
+      {
+        "<leader>cwf",
+        mode = { "n", "x" },
+        function()
+          require("wtf").fix()
+        end,
+        desc = "Fix diagnostic with AI",
+      },
+      {
+        mode = { "n" },
+        "<leader>cws",
+        function()
+          require("wtf").search()
+        end,
+        desc = "Search diagnostic with Google",
+      },
+      {
+        mode = { "n" },
+        "<leader>cwp",
+        function()
+          require("wtf").pick_provider()
+        end,
+        desc = "Pick provider",
+      },
+      {
+        mode = { "n" },
+        "<leader>cwh",
+        function()
+          require("wtf").history()
+        end,
+        desc = "Populate the quickfix list with previous chat history",
+      },
+      {
+        mode = { "n" },
+        "<leader>cwg",
+        function()
+          require("wtf").grep_history()
+        end,
+        desc = "Grep previous chat history with Telescope",
+      },
+    },
+  },
+}
