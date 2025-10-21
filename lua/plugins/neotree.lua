eea492892c1bf798b2680d1c013ae8876771789f
Kratosgado <mbeahessilfieprince@gmail.com>
2025-10-21 03:54:37 +0000

    use snack explorer

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,8 +1,25 @@
 return {
-  "nvim-neo-tree/neo-tree.nvim",
-  opts = {
-    window = {
-      position = "right", -- set Neo-tree to the right
+  -- "nvim-neo-tree/neo-tree.nvim",
+  -- opts = {
+  --   window = {
+  --     position = "right", -- set Neo-tree to the right
+  --   },
+  -- },
+  {
+    "folke/snacks.nvim",
+    ---@type snacks.Config
+    opts = {
+      picker = {
+        sources = {
+          explorer = {
+            layout = {
+              layout = {
+                position = "right",
+              },
+            },
+          },
+        },
+      },
     },
   },
 }
