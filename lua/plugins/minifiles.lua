7e825d2fb79da3239444940f98eb2a079c477cfc
Kratosgado <mbeahessilfieprince@gmail.com>
2025-10-29 05:22:37 +0000

    add tiny-inline-diagnostic

--- lua/plugins/minifiles.lua
+++ lua/plugins/minifiles.lua
@@ -13,4 +13,26 @@ return {
       },
     },
   },
+  {
+    {
+      "rachartier/tiny-inline-diagnostic.nvim",
+      event = "VeryLazy",
+      priority = 1000,
+      opts = {
+        multilines = {
+          enabled = true,
+          tabstop = 2,
+        },
+        show_source = {
+          enabled = true,
+        },
+
+        show_all_diags_on_cursorline = true,
+      },
+    },
+    {
+      "neovim/nvim-lspconfig",
+      opts = { diagnostics = { virtual_text = false } },
+    },
+  },
 }
