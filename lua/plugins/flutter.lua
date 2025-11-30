f9c3f8d24cedc8cadb44bb5ab41814c1cfb3400d
Kratosgado <mbeahessilfieprince@gmail.com>
2025-11-30 02:58:20 +0000

    add flutter

---
+++ lua/plugins/flutter.lua
@@ -0,0 +1,31 @@
+return {
+  {
+    "nvim-flutter/flutter-tools.nvim",
+    lazy = false,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "stevearc/dressing.nvim", -- optional for vim.ui.select
+    },
+    config = true,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        dartls = false,
+      },
+    },
+  },
+  {
+    "nvim-neotest/neotest",
+    optional = true,
+    dependencies = {
+      "sidlatau/neotest-dart",
+    },
+    opts = {
+      adapters = {
+        ["neotest-dart"] = {},
+      },
+    },
+  },
+}
