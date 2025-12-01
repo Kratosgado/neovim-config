9c46a5311e142f55c48ec94dd33887decc1d8fd1
Kratosgado <mbeahessilfieprince@gmail.com>
2025-12-01 11:52:49 +0000

    add java testing

--- lua/plugins/java.lua
+++ lua/plugins/java.lua
@@ -9,4 +9,45 @@ return {
     ---@type bootls.Config
     opts = {},
   },
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    opts = function()
+      -- Simple configuration to attach to remote java debug process
+      -- Taken directly from https://github.com/mfussenegger/nvim-dap/wiki/Java
+      local dap = require("dap")
+      dap.configurations.java = {
+        {
+          type = "java",
+          request = "attach",
+          name = "Debug (Attach) - Remote",
+          hostName = "127.0.0.1",
+          port = 5005,
+        },
+      }
+    end,
+    dependencies = {
+      {
+        "mason-org/mason.nvim",
+        opts = { ensure_installed = { "java-debug-adapter", "java-test" } },
+      },
+    },
+  },
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-neotest/nvim-nio",
+      "rcasia/neotest-java",
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    opts = {
+      adapters = {
+        ["neotest-java"] = {
+          -- config here
+        },
+      },
+    },
+  },
 }
