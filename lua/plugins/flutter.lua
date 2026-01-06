483304c17cfb8ab4db4a9aa22322070745309938
Kratosgado <mbeahessilfieprince@gmail.com>
2026-01-06 08:49:57 +0000

    add(master): pubspec assist

--- lua/plugins/flutter.lua
+++ lua/plugins/flutter.lua
@@ -30,6 +30,14 @@ return {
       })
     end,
   },
+  {
+    "akinsho/pubspec-assist.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("pubspec-assist").setup()
+    end,
+  },
+
   -- {
   --   "neovim/nvim-lspconfig",
   --   opts = {
