4511727ddac1644d97a2589294da3d29bc8c2e12
Kratosgado <mbeahessilfieprince@gmail.com>
2026-01-14 05:49:57 +0000

    remove buggy snap

--- lua/plugins/snap.lua
+++ lua/plugins/snap.lua
@@ -1,20 +1,20 @@
 return {
-  {
-    "mistricky/codesnap.nvim",
-    build = "make",
-    -- keys = {
-    --   { "<leader>cc", "<cmd>CodeSnap<cr>", mode = "x", desc = "Save selected code snapshot into clipboard" },
-    --   { "<leader>cs", "<cmd>CodeSnapSave<cr>", mode = "x", desc = "Save selected code snapshot in ~/Pictures" },
-    -- },
-    opts = {
-      breadcrumbs_separator = "/",
-      has_breadcrumbs = true,
-      has_line_number = false,
-      mac_window_bar = true,
-      save_path = "~/Pictures/codesnaps/",
-      bg_theme = "grape",
-      title = "CodeSnap.nvim",
-      watermark = "Kratosgado",
-    },
-  },
+  -- {
+  --   "mistricky/codesnap.nvim",
+  --   build = "make",
+  --   -- keys = {
+  --   --   { "<leader>cc", "<cmd>CodeSnap<cr>", mode = "x", desc = "Save selected code snapshot into clipboard" },
+  --   --   { "<leader>cs", "<cmd>CodeSnapSave<cr>", mode = "x", desc = "Save selected code snapshot in ~/Pictures" },
+  --   -- },
+  --   opts = {
+  --     breadcrumbs_separator = "/",
+  --     has_breadcrumbs = true,
+  --     has_line_number = false,
+  --     mac_window_bar = true,
+  --     save_path = "~/Pictures/codesnaps/",
+  --     bg_theme = "grape",
+  --     title = "CodeSnap.nvim",
+  --     watermark = "Kratosgado",
+  --   },
+  -- },
 }
