08c6c3f848d9bee9f0acc88d2dde0ec7de795954
Kratosgado <mbeahessilfieprince@gmail.com>
2026-01-29 06:58:05 +0000

    please sidekick

--- lua/plugins/opencode.lua
+++ lua/plugins/opencode.lua
@@ -1,51 +1,51 @@
 return {
-  {
-    "NickvanDyke/opencode.nvim",
-    dependencies = {
-      -- Recommended for `ask()` and `select()`.
-      -- Required for `snacks` provider.
-      ---@module 'snacks' <- Loads `snacks.nvim` types for configuration intellisense.
-      { "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } },
-    },
-    config = function()
-      ---@type opencode.Opts
-      -- vim.g.opencode_opts = {}
-      require("lualine").setup({
-        sections = {
-          lualine_z = {
-            {
-              require("opencode").statusline,
-            },
-          },
-        },
-      })
-      -- Required for `opts.events.reload`.
-      vim.o.autoread = true
-
-      -- Recommended/example keymaps.
-      vim.keymap.set({ "n", "x" }, "<leader>aa", function()
-        require("opencode").ask("@this: ", { submit = true })
-      end, { desc = "Ask opencode" })
-      vim.keymap.set({ "n", "x" }, "<leader>ae", function()
-        require("opencode").select()
-      end, { desc = "Execute opencode action…" })
-      vim.keymap.set({ "n", "t" }, "<leader>at", function()
-        require("opencode").toggle()
-      end, { desc = "Toggle opencode" })
-
-      vim.keymap.set({ "n", "x" }, "go", function()
-        return require("opencode").operator("@this ")
-      end, { expr = true, desc = "Add range to opencode" })
-      vim.keymap.set("n", "goo", function()
-        return require("opencode").operator("@this ") .. "_"
-      end, { expr = true, desc = "Add line to opencode" })
-
-      vim.keymap.set("n", "<C-u>", function()
-        require("opencode").command("session.half.page.up")
-      end, { desc = "opencode half page up" })
-      vim.keymap.set("n", "<C-d>", function()
-        require("opencode").command("session.half.page.down")
-      end, { desc = "opencode half page down" })
-    end,
-  },
+  -- {
+  --   "NickvanDyke/opencode.nvim",
+  --   dependencies = {
+  --     -- Recommended for `ask()` and `select()`.
+  --     -- Required for `snacks` provider.
+  --     ---@module 'snacks' <- Loads `snacks.nvim` types for configuration intellisense.
+  --     { "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } },
+  --   },
+  --   config = function()
+  --     ---@type opencode.Opts
+  --     -- vim.g.opencode_opts = {}
+  --     require("lualine").setup({
+  --       sections = {
+  --         lualine_z = {
+  --           {
+  --             require("opencode").statusline,
+  --           },
+  --         },
+  --       },
+  --     })
+  --     -- Required for `opts.events.reload`.
+  --     vim.o.autoread = true
+  --
+  --     -- Recommended/example keymaps.
+  --     vim.keymap.set({ "n", "x" }, "<leader>aa", function()
+  --       require("opencode").ask("@this: ", { submit = true })
+  --     end, { desc = "Ask opencode" })
+  --     vim.keymap.set({ "n", "x" }, "<leader>ae", function()
+  --       require("opencode").select()
+  --     end, { desc = "Execute opencode action…" })
+  --     vim.keymap.set({ "n", "t" }, "<leader>at", function()
+  --       require("opencode").toggle()
+  --     end, { desc = "Toggle opencode" })
+  --
+  --     vim.keymap.set({ "n", "x" }, "go", function()
+  --       return require("opencode").operator("@this ")
+  --     end, { expr = true, desc = "Add range to opencode" })
+  --     vim.keymap.set("n", "goo", function()
+  --       return require("opencode").operator("@this ") .. "_"
+  --     end, { expr = true, desc = "Add line to opencode" })
+  --
+  --     vim.keymap.set("n", "<C-u>", function()
+  --       require("opencode").command("session.half.page.up")
+  --     end, { desc = "opencode half page up" })
+  --     vim.keymap.set("n", "<C-d>", function()
+  --       require("opencode").command("session.half.page.down")
+  --     end, { desc = "opencode half page down" })
+  --   end,
+  -- },
 }
