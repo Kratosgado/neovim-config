283381ae619ae89d3cc5a2d7bdb2187e3b812df9
Kratosgado <mbeahessilfieprince@gmail.com>
2026-02-03 15:38:05 +0000

    java format

--- lua/plugins/java.lua
+++ lua/plugins/java.lua
@@ -5,7 +5,7 @@ return {
   },
   {
     "JavaHello/spring-boot.nvim",
-    ft = { "java", "yaml", "jproperties" },
+    ft = { "java", "jproperties" },
     dependencies = {
       "mfussenegger/nvim-jdtls", -- or nvim-java, nvim-lspconfig
       "ibhagwan/fzf-lua", -- 可选，用于符号选择等UI功能。也可以使用其他选择器（例如 telescope.nvim）。
