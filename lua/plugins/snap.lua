a64a3c28ca11e534101e1e0aa7e9fd58f7c27868
Kratosgado <mbeahessilfieprince@gmail.com>
2026-02-15 12:16:56 +0000

    git-auto-commit: /home/kratosgado/.config/nvim/lua/plugins/snap.lua

--- lua/plugins/snap.lua
+++ lua/plugins/snap.lua
@@ -1,29 +1,29 @@
 return {
   -- TODO: Fix codesnap
-  {
-    "mistricky/codesnap.nvim",
-    -- keys = {
-    --   { "<leader>cc", "<cmd>CodeSnap<cr>", mode = "x", desc = "Save selected code snapshot into clipboard" },
-    --   { "<leader>cs", "<cmd>CodeSnapSave<cr>", mode = "x", desc = "Save selected code snapshot in ~/Pictures" },
-    -- },
-    opts = {
-      snapshot_config = {
-        code_donfig = {
-          breadcrumbs = {
-            enable = true,
-            separator = "/",
-            color = "#80848b",
-            -- font_family = "ComicShannsMono Nerd Font",
-          },
-        },
-      },
-      show_line_number = false,
-      save_path = "~/Pictures/codesnaps/",
-      watermark = {
-        content = "Kratosgado",
-        -- font_family = "ComicShannsMono Nerd Font",
-      },
-      clipboard = false,
-    },
-  },
+  -- {
+  --   "mistricky/codesnap.nvim",
+  --   -- keys = {
+  --   --   { "<leader>cc", "<cmd>CodeSnap<cr>", mode = "x", desc = "Save selected code snapshot into clipboard" },
+  --   --   { "<leader>cs", "<cmd>CodeSnapSave<cr>", mode = "x", desc = "Save selected code snapshot in ~/Pictures" },
+  --   -- },
+  --   opts = {
+  --     snapshot_config = {
+  --       code_donfig = {
+  --         breadcrumbs = {
+  --           enable = true,
+  --           separator = "/",
+  --           color = "#80848b",
+  --           -- font_family = "ComicShannsMono Nerd Font",
+  --         },
+  --       },
+  --     },
+  --     show_line_number = false,
+  --     save_path = "~/Pictures/codesnaps/",
+  --     watermark = {
+  --       content = "Kratosgado",
+  --       -- font_family = "ComicShannsMono Nerd Font",
+  --     },
+  --     clipboard = false,
+  --   },
+  -- },
 }
