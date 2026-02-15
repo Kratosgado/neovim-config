a350116b0214662abd18711414bf15e3152995a4
Kratosgado <mbeahessilfieprince@gmail.com>
2026-02-15 09:17:57 +0000

    git-auto-commit: /home/kratosgado/.config/nvim/lua/plugins/snap.lua

--- lua/plugins/snap.lua
+++ lua/plugins/snap.lua
@@ -1,7 +1,7 @@
 return {
   {
     "mistricky/codesnap.nvim",
-    tag = "^2",
+    tag = "^2.0.0",
     -- keys = {
     --   { "<leader>cc", "<cmd>CodeSnap<cr>", mode = "x", desc = "Save selected code snapshot into clipboard" },
     --   { "<leader>cs", "<cmd>CodeSnapSave<cr>", mode = "x", desc = "Save selected code snapshot in ~/Pictures" },
