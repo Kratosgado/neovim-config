a687743b4887ad6b681a7b5a879ada4b41b75383
Kratosgado <mbeahessilfieprince@gmail.com>
2026-02-15 13:20:36 +0000

    git-auto-commit: /home/kratosgado/.config/nvim/lua/plugins/test-coverage.lua

--- lua/plugins/test-coverage.lua
+++ lua/plugins/test-coverage.lua
@@ -1,29 +1,4 @@
 return {
-  -- {
-  --   "dsych/blanket.nvim",
-  --   ft = "java",
-  --   opts = {
-  --     report_path = vim.fn.getcwd() .. "/target/site/jacoco/jacoco.xml",
-  --     filetypes = "java",
-  --     igns = {
-  --       priority = 10,
-  --       incomplete_branch_color = "WarningMsg",
-  --       covered_color = "DiagnosticOk",
-  --       uncovered_color = "Error",
-  --     },
-  --   },
-  --   keys = {
-  --     { "<leader>tcp", "<cmd>lua require('blanket').pick_report_path()<CR>", desc = "Pick JaCoCo Report" },
-  --     { "<leader>tcc", "<cmd>lua require('blanket').refresh()<cr>", desc = "Refresh Coverage" },
-  --     { "<leader>tcS", "<cmd>lua require('blanket').start()<cr>", desc = "Start Blanket" },
-  --     { "<leader>tcX", "<cmd>lua require('blanket').stop()<cr>", desc = "Stop Blanket" },
-  --     {
-  --       "<leader>tcr",
-  --       "<cmd>TermExec cmd='mvn test jacoco:report'<cr>",
-  --       desc = "Maven: Run Tests & Coverage",
-  --     },
-  --   },
-  -- },
   {
     "andythigpen/nvim-coverage",
     dependencies = { "nvim-lua/plenary.nvim", "nvim-neotest/neotest" },
