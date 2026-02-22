41841f9778508305dd1a03184cca4ac0ddd8655a
Kratosgado <mbeahessilfieprince@gmail.com>
2026-02-22 15:32:10 +0000

    git-auto-commit: /home/kratosgado/.config/nvim/lua/config/autocmds.lua

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -77,6 +77,17 @@ vim.api.nvim_create_autocmd("User", {
   end,
 })
 
+-- Obsidian
+vim.api.nvim_create_autocmd("User", {
+  pattern = "ObsidianNoteEnter",
+  callback = function(ev)
+    vim.keymap.set("n", "<leader>ch", "<cmd>Obsidian toggle_checkbox<cr>", {
+      buffer = true,
+      desc = "Toggle checkbox",
+    })
+  end,
+})
+
 -- Open PDFs with PDFview
 vim.api.nvim_create_autocmd("BufReadPost", {
   pattern = "*.pdf",
