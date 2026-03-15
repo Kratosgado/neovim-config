f333cd3e7926cb0d3f32c3db8e186e15c2c7f75b
Kratosgado <mbeahessilfieprince@gmail.com>
2026-03-15 19:03:59 +0000

    git-auto-commit: /home/esslifie/.config/nvim/lua/plugins/rust.lua

--- lua/plugins/rust.lua
+++ lua/plugins/rust.lua
@@ -119,7 +119,7 @@ return {
   {
     "cordx56/rustowl",
     version = "*", -- Latest stable version
-    -- build = "cargo binstall rustowl",
+    build = "cargo binstall rustowl",
     lazy = false, -- This plugin is already lazy
     opts = {
       client = {
