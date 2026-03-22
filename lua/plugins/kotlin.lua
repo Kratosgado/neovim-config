9c43702f2b71fb2c8fb2d46891fecd71a255af2e
Kratosgado <mbeahessilfieprince@gmail.com>
2026-03-22 09:52:38 +0000

    git-auto-commit: /home/kratosgado/.config/nvim/lua/plugins/kotlin.lua

--- lua/plugins/kotlin.lua
+++ lua/plugins/kotlin.lua
@@ -54,45 +54,45 @@ return {
   },
 
   -- 6. Your existing Debug Adapter (DAP) configuration
-  {
-    "mfussenegger/nvim-dap",
-    optional = true,
-    dependencies = "mason-org/mason.nvim",
-    opts = function()
-      local dap = require("dap")
-      if not dap.adapters.kotlin then
-        dap.adapters.kotlin = {
-          type = "executable",
-          command = "kotlin-debug-adapter",
-          options = { auto_continue_if_many_stopped = false },
-        }
-      end
-
-      dap.configurations.kotlin = {
-        {
-          type = "kotlin",
-          request = "launch",
-          name = "This file",
-          mainClass = function()
-            local root = vim.fs.find("src", { path = vim.uv.cwd(), upward = true, stop = vim.env.HOME })[1] or ""
-            local fname = vim.api.nvim_buf_get_name(0)
-            return fname:gsub(root, ""):gsub("main/kotlin/", ""):gsub(".kt", "Kt"):gsub("/", "."):sub(2, -1)
-          end,
-          projectRoot = "${workspaceFolder}",
-          jsonLogFile = "",
-          enableJsonLogging = false,
-        },
-        {
-          type = "kotlin",
-          request = "attach",
-          name = "Attach to debugging session",
-          port = 5005,
-          args = {},
-          projectRoot = vim.fn.getcwd,
-          hostName = "localhost",
-          timeout = 2000,
-        },
-      }
-    end,
-  },
+  -- {
+  --   "mfussenegger/nvim-dap",
+  --   optional = true,
+  --   dependencies = "mason-org/mason.nvim",
+  --   opts = function()
+  --     local dap = require("dap")
+  --     if not dap.adapters.kotlin then
+  --       dap.adapters.kotlin = {
+  --         type = "executable",
+  --         command = "kotlin-debug-adapter",
+  --         options = { auto_continue_if_many_stopped = false },
+  --       }
+  --     end
+  --
+  --     dap.configurations.kotlin = {
+  --       {
+  --         type = "kotlin",
+  --         request = "launch",
+  --         name = "This file",
+  --         mainClass = function()
+  --           local root = vim.fs.find("src", { path = vim.uv.cwd(), upward = true, stop = vim.env.HOME })[1] or ""
+  --           local fname = vim.api.nvim_buf_get_name(0)
+  --           return fname:gsub(root, ""):gsub("main/kotlin/", ""):gsub(".kt", "Kt"):gsub("/", "."):sub(2, -1)
+  --         end,
+  --         projectRoot = "${workspaceFolder}",
+  --         jsonLogFile = "",
+  --         enableJsonLogging = false,
+  --       },
+  --       {
+  --         type = "kotlin",
+  --         request = "attach",
+  --         name = "Attach to debugging session",
+  --         port = 5005,
+  --         args = {},
+  --         projectRoot = vim.fn.getcwd,
+  --         hostName = "localhost",
+  --         timeout = 2000,
+  --       },
+  --     }
+  --   end,
+  -- },
 }
