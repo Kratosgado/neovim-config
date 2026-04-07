eeb010b0832501990e5b4ec6fe81599f0bd40558
Kratosgado <mbeahessilfieprince@gmail.com>
2026-04-07 20:40:52 +0000

    git-auto-commit: /home/esslifie/.config/nvim/lua/plugins/vue.lua

--- lua/plugins/vue.lua
+++ lua/plugins/vue.lua
@@ -19,27 +19,28 @@ return {
             },
           },
         },
-        -- vue_ls = false,
-        -- vtsls = {},
+        vue_ls = false,
+        vtsls = {},
       },
     },
   },
-  -- {
-  --   "neovim/nvim-lspconfig",
-  --   opts = function(_, opts)
-  --     -- Initialize the table if it doesn't exist to prevent the 'got nil' error
-  --     opts.servers.vtsls.filetypes = opts.servers.vtsls.filetypes or {}
-  --     table.insert(opts.servers.vtsls.filetypes, "vue")
-  --
-  --     LazyVim.extend(opts.servers.vtsls, "settings.vtsls.tsserver.globalPlugins", {
-  --       {
-  --         name = "@vue/typescript-plugin",
-  --         location = LazyVim.get_pkg_path("vue-language-server", "/node_modules/@vue/language-server"),
-  --         languages = { "vue" },
-  --         configNamespace = "typescript",
-  --         enableForWorkspaceTypeScriptVersions = true,
-  --       },
-  --     })
-  --   end,
-  -- },
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      -- Initialize the table if it doesn't exist to prevent the 'got nil' error
+      opts.servers.vtsls.filetypes = opts.servers.vtsls.filetypes or {}
+      table.insert(opts.servers.vtsls.filetypes, "vue")
+
+      LazyVim.extend(opts.servers.vtsls, "settings.vtsls.tsserver.globalPlugins", {
+        {
+          name = "@vue/typescript-plugin",
+          location = LazyVim.get_pkg_path("vue-language-server", "/node_modules/@vue/language-server"),
+          languages = { "vue" },
+          configNamespace = "typescript",
+          enableForWorkspaceTypeScriptVersions = true,
+        },
+      })
+    end,
+  },
+,
 }
