010d5a44e42b2affb1b96f64a3dc6b1f397599ab
Kratosgado <mbeahessilfieprince@gmail.com>
2026-04-08 07:13:16 +0000

    git-auto-commit: /home/esslifie/.config/nvim/lua/plugins/vue.lua

--- lua/plugins/vue.lua
+++ lua/plugins/vue.lua
@@ -4,97 +4,43 @@ return {
     opts = { ensure_installed = { "vue", "css" } },
   },
   {
+
     "mason-org/mason.nvim",
-    opts = { ensure_installed = { "vue-language-server", "vtsls" } },
+    opts = { ensure_installed = { "vue-language-server" } },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        volar = {
+          init_options = {
+            vue = {
+              hybridMode = true,
+            },
+          },
+        },
+        vue_ls = false,
+        vtsls = {},
+      },
+    },
   },
   {
     "neovim/nvim-lspconfig",
     opts = function(_, opts)
-      local util = require("lspconfig.util")
+      -- Initialize the table if it doesn't exist to prevent the 'got nil' error
+      opts.servers.vtsls.filetypes = opts.servers.vtsls.filetypes or {}
+      table.insert(opts.servers.vtsls.filetypes, "vue")
 
-      local function is_vue_project(fname)
-        return util.root_pattern(
-          "nuxt.config.ts",
-          "nuxt.config.js",
-          "nuxt.config.mts",
-          "vue.config.ts",
-          "vue.config.js"
-        )(fname)
-      end
-      local function is_frontend_project(fname)
-        return util.root_pattern(
-          -- Vue / Nuxt
-          "nuxt.config.ts",
-          "nuxt.config.js",
-          "nuxt.config.mts",
-          "vue.config.ts",
-          "vue.config.js",
-          -- React / Next
-          "next.config.ts",
-          "next.config.js",
-          "next.config.mjs",
-          -- Svelte
-          "svelte.config.js",
-          "svelte.config.ts",
-          -- Astro
-          "astro.config.ts",
-          "astro.config.js",
-          "astro.config.mjs",
-          -- Generic signal — tailwind config itself
-          "tailwind.config.ts",
-          "tailwind.config.js",
-          "tailwind.config.mjs"
-        )(fname)
-      end
-
-      -- Volar for .vue files
-      opts.servers.volar = {
-        init_options = {
-          vue = { hybridMode = true },
+      LazyVim.extend(opts.servers.vtsls, "settings.vtsls.tsserver.globalPlugins", {
+        {
+          name = "@vue/typescript-plugin",
+          location = LazyVim.get_pkg_path("vue-language-server", "/node_modules/@vue/language-server"),
+          languages = { "vue" },
+          configNamespace = "typescript",
+          enableForWorkspaceTypeScriptVersions = true,
         },
-      }
-      opts.servers.tailwindcss = {
-        root_dir = function(fname)
-          if not is_frontend_project(fname) then
-            return nil
-          end
-          return util.root_pattern("tailwind.config.ts", "tailwind.config.js", "tailwind.config.mjs", "package.json")(
-            fname
-          )
-        end,
-      }
-      -- vtsls with vue plugin, only in vue/nuxt projects
-      opts.servers.vtsls = {
-        filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
-        root_dir = function(fname)
-          return is_vue_project(fname)
-        end,
-        settings = {
-          vtsls = {
-            tsserver = {
-              globalPlugins = {
-                {
-                  name = "@vue/typescript-plugin",
-                  location = LazyVim.get_pkg_path("vue-language-server", "/node_modules/@vue/language-server"),
-                  languages = { "vue" },
-                  configNamespace = "typescript",
-                  enableForWorkspaceTypeScriptVersions = true,
-                },
-              },
-            },
-          },
-        },
-      }
-
-      -- disable tsgo in vue/nuxt projects
-      opts.servers.tsgo = {
-        root_dir = function(fname)
-          if is_vue_project(fname) then
-            return nil
-          end
-          return util.root_pattern("tsconfig.json", "package.json", "jsconfig.json", ".git")(fname)
-        end,
-      }
+      })
     end,
   },
+,
 }
