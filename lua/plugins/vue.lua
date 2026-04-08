f36630c233faba9873f36233a98c9df0c38158d9
Kratosgado <mbeahessilfieprince@gmail.com>
2026-04-08 07:39:39 +0000

    git-auto-commit: /home/esslifie/.config/nvim/lua/plugins/vue.lua

--- lua/plugins/vue.lua
+++ lua/plugins/vue.lua
@@ -1,35 +1,3 @@
-local util = require("lspconfig.util")
-
-local function is_vue_project(fname)
-  return util.root_pattern("nuxt.config.ts", "nuxt.config.js", "nuxt.config.mts", "vue.config.ts", "vue.config.js")(
-    fname
-  )
-end
-local function is_frontend_project(fname)
-  return util.root_pattern(
-    -- Vue / Nuxt
-    "nuxt.config.ts",
-    "nuxt.config.js",
-    "nuxt.config.mts",
-    "vue.config.ts",
-    "vue.config.js",
-    -- React / Next
-    "next.config.ts",
-    "next.config.js",
-    "next.config.mjs",
-    -- Svelte
-    "svelte.config.js",
-    "svelte.config.ts",
-    -- Astro
-    "astro.config.ts",
-    "astro.config.js",
-    "astro.config.mjs",
-    -- Generic signal — tailwind config itself
-    "tailwind.config.ts",
-    "tailwind.config.js",
-    "tailwind.config.mjs"
-  )(fname)
-end
 return {
   {
     "nvim-treesitter/nvim-treesitter",
@@ -52,36 +20,23 @@ return {
           },
         },
         vue_ls = false,
-        tailwindcss = {
-          root_dir = function(fname)
-            return is_frontend_project(fname)
-          end,
-        },
-        vtsls = {
-          root_dir = function(fname)
-            return is_vue_project(fname)
-          end,
-        },
+        vtsls = {},
       },
     },
   },
   {
     "neovim/nvim-lspconfig",
     opts = function(_, opts)
-      if opts.servers.vtsls then
-        table.insert(opts.servers.vtsls.filetypes, "vue")
-        LazyVim.extend(opts.servers.vtsls, "settings.vtsls.tsserver.globalPlugins", {
-          {
-            name = "@vue/typescript-plugin",
-            location = LazyVim.get_pkg_path("vue-language-server", "/node_modules/@vue/language-server"),
-            languages = { "vue" },
-            configNamespace = "typescript",
-            enableForWorkspaceTypeScriptVersions = true,
-          },
-        })
-      else
-        vim.g.lazyvim_ts_lsp = "tsgo"
-      end
+      table.insert(opts.servers.vtsls.filetypes, "vue")
+      LazyVim.extend(opts.servers.vtsls, "settings.vtsls.tsserver.globalPlugins", {
+        {
+          name = "@vue/typescript-plugin",
+          location = LazyVim.get_pkg_path("vue-language-server", "/node_modules/@vue/language-server"),
+          languages = { "vue" },
+          configNamespace = "typescript",
+          enableForWorkspaceTypeScriptVersions = true,
+        },
+      })
     end,
   },
 }
