aa5c76cca3561144a4ca26c9a75f7da194ee18fb
Kratosgado <mbeahessilfieprince@gmail.com>
2026-05-02 22:16:03 +0000

    git-auto-commit: /home/esslifie/.config/nvim/lua/user/vscode_keymaps.lua

--- lua/user/vscode_keymaps.lua
+++ lua/user/vscode_keymaps.lua
@@ -2,7 +2,7 @@ local keymap = vim.keymap.set
 local opts = { noremap = true, silent = true }
 
 -- remap leader key
-keymap("n", "<Space>", "", opts)
+-- keymap("n", "<Space>", "", opts)
 -- vim.g.mapleader = " "
 -- vim.g.maplocalleader = " "
 
