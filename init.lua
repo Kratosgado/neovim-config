c0ef29ca2a1a6ea685cfc2415aade38a6dde3a1f
Kratosgado <mbeahessilfieprince@gmail.com>
2026-06-13 11:41:42 +0000

    git-auto-commit: /home/esslifie/.config/nvim/init.lua

--- init.lua
+++ init.lua
@@ -4,5 +4,10 @@ if vim.g.vscode then
   -- require("user.vscode_keymaps")
 
   vim.o.cmdheight = 20
+elseif vim.g.neovide then
+  vim.o.guifont = "JetBrainsMono Nerd Font:h12"
+
+  vim.g.neovide_cursor_vfx_mode = "railgun"
+  vim.g.neovide_scroll_animation_length = 0.2
 end
 require("config.lazy")
