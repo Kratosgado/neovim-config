3020822ed5d4fe52af41a96168311c6935a33eeb
Kratosgado <mbeahessilfieprince@gmail.com>
2026-06-13 12:13:15 +0000

    git-auto-commit: /home/esslifie/.config/nvim/init.lua

--- init.lua
+++ init.lua
@@ -5,6 +5,7 @@ if vim.g.vscode then
 
   vim.o.cmdheight = 20
 elseif vim.g.neovide then
+  vim.env.PATH = vim.env.PATH .. ":/usr/bin:/usr/local/bin:" .. vim.env.HOME .. "/.fzf/bin"
   vim.o.guifont = "ComicShannsMono Nerd Font:h8"
 
   vim.g.neovide_cursor_vfx_mode = "railgun"
