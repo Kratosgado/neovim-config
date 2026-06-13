c618cadc2eb1993c21c950baf16728fd5b8a6a6d
Kratosgado <mbeahessilfieprince@gmail.com>
2026-06-13 12:33:24 +0000

    git-auto-commit: /home/esslifie/.config/nvim/init.lua

--- init.lua
+++ init.lua
@@ -8,7 +8,7 @@ elseif vim.g.neovide then
   -- vim.env.PATH = vim.env.PATH .. ":/usr/bin:/usr/local/bin:" .. vim.env.HOME .. "/.fzf/bin"
 
   vim.o.guifont = "ComicShannsMono Nerd Font:h8"
-
+  vim.g.neovide_title_hidden = true
   vim.g.neovide_cursor_vfx_mode = "railgun"
   vim.g.neovide_scroll_animation_length = 0.2
 end
