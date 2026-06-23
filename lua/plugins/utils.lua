0bf569fe00aa89470fc7d30ce80c25e006d96e4e
Kratosgado <mbeahessilfieprince@gmail.com>
2026-06-23 07:52:21 +0000

    git-auto-commit: /home/esslifie/.config/nvim/lua/plugins/utils.lua

--- lua/plugins/utils.lua
+++ lua/plugins/utils.lua
@@ -70,12 +70,4 @@ return {
       },
     },
   },
-  {
-    "oribarilan/lensline.nvim",
-    tag = "release/2.x", -- or: branch = 'release/2.x' for latest non-breaking updates
-    event = "LspAttach",
-    config = function()
-      require("lensline").setup()
-    end,
-  },
 }
