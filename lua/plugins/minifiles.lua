db8ec8b387d792bd6a4a4a43a561fd097cc890a0
Kratosgado <mbeahessilfieprince@gmail.com>
2026-06-25 14:02:47 +0000

    git-auto-commit: /home/esslifie/.config/nvim/lua/plugins/minifiles.lua

--- lua/plugins/minifiles.lua
+++ lua/plugins/minifiles.lua
@@ -21,17 +21,28 @@ return {
     opts = {
       windows = {
         preview = true,
-        width_focus = 30, -- Width of the focused window
+        width_focus = 30,
         width_preview = 80,
       },
       options = {
         use_as_default_explorer = true,
       },
       mappings = {
-        close = "jk", -- Close file explorer
-        open = "<CR>", -- Open file
+        close = "jk",
+        open = "<CR>",
       },
     },
+    config = function(_, opts)
+      require("mini.files").setup(opts)
+      vim.api.nvim_create_autocmd("VimEnter", {
+        callback = function()
+          local arg = vim.fn.argv(0)
+          if arg and vim.fn.isdirectory(arg) == 1 then
+            require("mini.files").open(arg, true)
+          end
+        end,
+      })
+    end,
   },
   {
     "nvim-mini/mini.diff",
