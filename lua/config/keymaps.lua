058edb4d94033b3b0b9062a404fc377afcafbd60
Kratosgado <mbeahessilfieprince@gmail.com>
2026-06-27 15:07:19 +0000

    git-auto-commit: /home/esslifie/.config/nvim/lua/config/keymaps.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -18,22 +18,22 @@ end, { expr = true, noremap = true, silent = true })
 map("n", "<leader>Co", "<cmd>CompilerOpen<cr>", { noremap = true, silent = true })
 map("n", "<leader>Cr", "<cmd>CompilerStop<cr>" .. "<cmd>CompilerRedo<cr>", { noremap = true, silent = true })
 map("n", "<leader>Ct", "<cmd>CompilerToggleResults<cr>", { noremap = true, silent = true })
-
--- Option 1: Simple mapping with fixed folder + timestamped filename
-vim.keymap.set({ "n", "x" }, "<leader>cS", function()
-  local timestamp = os.date("%Y%m%d-%H%M%S")
-  local path = vim.fn.expand("~/Pictures/codesnaps/snapshot-" .. timestamp .. ".png")
-  vim.cmd("CodeSnapSave " .. vim.fn.shellescape(path))
-end, { desc = "CodeSnap: Save with timestamp" })
-
--- Option 2: Prompt for filename (more flexible)
-vim.keymap.set({ "n", "x" }, "<leader>cs", function()
-  vim.ui.input({
-    prompt = "Save snapshot as (full path, e.g. ~/Pictures/code.png): ",
-    completion = "file", -- tab-complete paths
-  }, function(input)
-    if input and input ~= "" then
-      vim.cmd("CodeSnapSave " .. vim.fn.shellescape(input))
-    end
-  end)
-end, { desc = "CodeSnap: Save (prompt)" })
+--
+-- -- Option 1: Simple mapping with fixed folder + timestamped filename
+-- vim.keymap.set({ "n", "x" }, "<leader>cS", function()
+--   local timestamp = os.date("%Y%m%d-%H%M%S")
+--   local path = vim.fn.expand("~/Pictures/codesnaps/snapshot-" .. timestamp .. ".png")
+--   vim.cmd("CodeSnapSave " .. vim.fn.shellescape(path))
+-- end, { desc = "CodeSnap: Save with timestamp" })
+--
+-- -- Option 2: Prompt for filename (more flexible)
+-- vim.keymap.set({ "n", "x" }, "<leader>cs", function()
+--   vim.ui.input({
+--     prompt = "Save snapshot as (full path, e.g. ~/Pictures/code.png): ",
+--     completion = "file", -- tab-complete paths
+--   }, function(input)
+--     if input and input ~= "" then
+--       vim.cmd("CodeSnapSave " .. vim.fn.shellescape(input))
+--     end
+--   end)
+-- end, { desc = "CodeSnap: Save (prompt)" })
