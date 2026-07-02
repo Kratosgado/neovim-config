5d4638a6580467011112e0db0c4803d3a83c759e
Kratosgado <mbeahessilfieprince@gmail.com>
2026-07-02 08:21:16 +0000

    git-auto-commit: /home/esslifie/.config/nvim/install.sh

--- install.sh
+++ install.sh
@@ -5,19 +5,11 @@ mkdir -p ~/.config/nvim
 # Sync current directory content to nvim config (excluding the script itself if desired)
 cp -rf . ~/.config/nvim
 
-# 2. Setup Zsh Symlinks
-# Remove existing files/dirs to avoid link conflicts
+# 2. Setup Zsh Symlink
 rm -f ~/.zshrc
-rm -rf ~/.oh-my-zsh/custom
-
-# Link the zshrc from the nvim folder to the home directory
 ln -s ~/.config/nvim/zshrc ~/.zshrc
 
-# Link the custom plugins/themes folder back to Oh My Zsh
-# This assumes you moved your 'custom' folder to 'zsh_custom' inside your nvim repo
-ln -s ~/.config/nvim/zsh_custom ~/.oh-my-zsh/custom
-
-# 3. Setup Kitty Symlink
+# 4. Setup Kitty Symlink
 rm -rf ~/.config/kitty
 ln -s ~/.config/nvim/kitty ~/.config/kitty
 
