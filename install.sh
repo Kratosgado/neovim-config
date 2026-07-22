ed536040cf91fc4f17a4344eb4acb6b7bf8985c5
Kratosgado <mbeahessilfieprince@gmail.com>
2026-07-22 19:12:11 +0000

    git-auto-commit: git_sync

--- install.sh
+++ install.sh
@@ -13,4 +13,8 @@ ln -s ~/.config/nvim/zshrc ~/.zshrc
 rm -rf ~/.config/kitty
 ln -s ~/.config/nvim/kitty ~/.config/
 
+# 5. Setup Claude Symlink
+rm -rf ~/.claude
+ln -s ~/.config/nvim/claude ~/.claude
+
 echo "Setup complete! Please restart your terminal or run 'source ~/.zshrc'"
