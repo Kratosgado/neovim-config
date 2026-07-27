affb4b34e07cb244f289da97e4933066e704aabc
Kratosgado <mbeahessilfieprince@gmail.com>
2026-07-27 17:52:54 +0000

    git-auto-commit: /home/esslifie/.config/nvim/lua/plugins/snacks.lua

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -12,6 +12,7 @@ return {
       image = { enabled = true },
       explorer = { enabled = false },
       quickfile = { enabled = true },
+      lazygit = { enabled = true, configure = true },
       picker = {
         enabled = false,
         sources = {
