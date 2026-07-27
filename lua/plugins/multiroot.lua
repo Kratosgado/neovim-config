7fad19e59dc8260920cb0b9745dfdd6146b36ccd
Kratosgado <mbeahessilfieprince@gmail.com>
2026-07-27 15:38:48 +0000

    git-auto-commit: /home/esslifie/.config/nvim/lua/plugins/multiroot.lua

--- lua/plugins/multiroot.lua
+++ lua/plugins/multiroot.lua
@@ -6,15 +6,16 @@ return {
     event = "VeryLazy",
     opts = {
       keys_when_active = {
-        { "<leader>qc", ":WorkspaceClose<cr>", desc = "Workspace: close" },
+        { "<leader>mc", ":WorkspaceClose<cr>", desc = "Workspace: close" },
         { "<leader><leader>", ":WorkspaceFiles<cr>", desc = "Workspace: files" },
         { "<leader>/", ":WorkspaceGrep<cr>", desc = "Workspace: grep" },
-        { "<leader>qa", ":WorkspaceAddFolder<cr>", desc = "Workspace: add folder" },
-        { "<leader>qi", ":WorkspaceList<cr>", desc = "Workspace: info" },
+        { "<leader>ma", ":WorkspaceAddFolder<cr>", desc = "Workspace: add folder" },
+        { "<leader>mi", ":WorkspaceList<cr>", desc = "Workspace: info" },
         { "<C-/>", ":WorkspaceTerm<cr>", desc = "Workspace: terminal in folder" },
-        { "<leader>qt", ":WorkspaceTermRun<cr>", desc = "Workspace: run named terminal" },
-        { "<leader>qx", ":WorkspaceTask<cr>", desc = "Workspace: run task" },
-        { "<leader>qe", ":WorkspaceEdit<cr>", desc = "Workspace: edit file" },
+        { "<leader>mt", ":WorkspaceTermRun<cr>", desc = "Workspace: run named terminal" },
+        { "<leader>mx", ":WorkspaceTask<cr>", desc = "Workspace: run task" },
+        { "<leader>me", ":WorkspaceEdit<cr>", desc = "Workspace: edit file" },
+        { "<leader>ml", ":WorkspaceReload<cr>", desc = "Workspace: reload" },
         { "<leader>gg", ":WorkspaceGit<cr>", desc = "Workspace: Neogit at buffer's folder" },
       },
       git = {
@@ -27,8 +28,38 @@ return {
       },
     },
     keys = {
-      { "<leader>qo", ":WorkspaceOpen<cr>", desc = "Workspace: open" },
-      { "<leader>qr", ":WorkspaceRecent<cr>", desc = "Workspace: recent" },
+      { "<leader>mo", ":WorkspaceOpen<cr>", desc = "Workspace: open" },
+      { "<leader>mr", ":WorkspaceRecent<cr>", desc = "Workspace: recent" },
+    },
+  },
+
+  -- statusline: show current workspace in lualine
+  {
+    "nvim-lualine/lualine.nvim",
+    optional = true,
+    opts = function(_, opts)
+      opts.sections = opts.sections or {}
+      opts.sections.lualine_c = opts.sections.lualine_c or {}
+      table.insert(opts.sections.lualine_c, {
+        function()
+          return require("multiroot").statusline()
+        end,
+        cond = function()
+          local ok, mr = pcall(require, "multiroot")
+          return ok and mr.current() ~= nil
+        end,
+      })
+    end,
+  },
+
+  -- which-key group label for the workspace prefix
+  {
+    "folke/which-key.nvim",
+    optional = true,
+    opts = {
+      spec = {
+        { "<leader>m", group = "workspace", icon = "" },
+      },
     },
   },
 }
